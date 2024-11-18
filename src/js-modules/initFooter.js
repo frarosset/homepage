ae968003429ec5f4b0bf0bcd93dc1dde305cc865
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-18 18:24:09 +0100

    Add dynamic content to footer via js
    Contact and social links are grabbed from the info.json file in
    the customData folder.

---
+++ src/js-modules/initFooter.js
@@ -0,0 +1,28 @@
+import info from "../customData/info.json";
+import portraitImg from "../customData/portrait.png";
+import initIconLinkList from "./initIconLinkList.js";
+
+export default function initFooter() {
+  // Address
+  const address = document.querySelector("footer .contact_me__address");
+  address.textContent = info.contact.address;
+
+  // Phone
+  const phone = document.querySelector("footer .contact_me__phone");
+  phone.textContent = info.contact.phone;
+
+  // Email
+  const email = document.querySelector("footer .contact_me__email");
+  email.textContent = info.contact.email;
+
+  // Social list
+  const socialListUl = document.querySelector(
+    "footer .contact-me__social-list"
+  );
+  initIconLinkList(Object.entries(info.social), socialListUl);
+
+  // Portrait image
+  const portrait = document.querySelector("footer .portrait");
+  portrait.src = portraitImg;
+  portrait.alt = `Portrait image placeholder for ${info.name}`;
+}
--- src/js-modules/initWebpage.js
+++ src/js-modules/initWebpage.js
@@ -1,7 +1,9 @@
 import initHeader from "./initHeader.js";
 import initMain from "./initMain.js";
+import initFooter from "./initFooter.js";
 
 export default function initWebpage() {
   initHeader();
   initMain();
+  initFooter();
 }
