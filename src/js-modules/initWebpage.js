3239458f9b0af04a1614f7251b25d080d1d62f5c
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-19 02:50:09 +0100

    Add credit footer

--- src/js-modules/initWebpage.js
+++ src/js-modules/initWebpage.js
@@ -1,9 +1,11 @@
 import initHeader from "./initHeader.js";
 import initMain from "./initMain.js";
 import initFooter from "./initFooter.js";
+import initCreditFooter from "../js-utilities/creditFooter.js";
 
 export default function initWebpage() {
   initHeader();
   initMain();
   initFooter();
+  initCreditFooter();
 }
